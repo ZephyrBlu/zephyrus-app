ef94b1e99b99a08577b7d34abc5e2625ce2628e2
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-09 21:22:29 +1200

    Update ESLint configuration

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,13 +1,35 @@
 module.exports = {
   use: [
-    '@neutrinojs/airbnb',
+    ['@neutrinojs/airbnb', {
+      eslint: {
+        rules: {
+          "linebreak-style": ["error", "windows"],
+          "indent": ["error", 4, { "SwitchCase": 1 }],
+          "react/jsx-indent": ["error", 4],
+          "react/jsx-indent-props": ["error", 4],
+          "react/prop-types": 0,
+          "react/no-multi-comp": 0,
+          "no-console": "off",
+          "max-len": 0,
+          "jsx-a11y/mouse-events-have-key-events": 0,
+          "jsx-a11y/label-has-for": { "some": [ "nesting", "id" ] },
+        }
+      }
+    }],
     [
       '@neutrinojs/react',
       {
         html: {
-          title: 'zephyrus-app'
+          title: 'Zephyrus | SC2 Replay Analysis'
         }
       }
-    ]
+    ],
+    (neutrino) => {
+      neutrino.config.module
+        .rule('postcss')
+          .test(/\.css$/)
+          .use('postcss')
+            .loader('postcss-loader');
+    },
   ]
 };
