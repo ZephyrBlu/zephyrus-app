189444bf1edbe0d38d352432c6147523069cf387
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-12 14:38:54 +1200

    Update dependencies and rename profile sections

--- src/Tabs/ReplayAnalysis.jsx
+++
@@ -1,7 +0,0 @@
-const ReplayAnalysis = () => (
-    <div>
-        <h1>Replay Analysis</h1>
-    </div>
-);
-
-export default ReplayAnalysis;
--- src/Tabs/ReplayList.jsx
+++
@@ -1,7 +0,0 @@
-const ReplayList = () => (
-    <div>
-        <h1>Replays</h1>
-    </div>
-);
-
-export default ReplayList;
