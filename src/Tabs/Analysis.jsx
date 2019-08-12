88078e2b9470136d8a8a03974060825f49fb9df1
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-12 18:10:30 +1200

    Refactor Analysis section for ProfileSection component + Misc
    Also moved some leftover CSS styles and formatting from the section components to ProfileSection to generalize styles.

--- src/Tabs/Analysis.jsx
+++ src/Tabs/Analysis.jsx
@@ -1,7 +1,21 @@
-const Analysis = () => (
-    <div>
-        <h1>Replay Analysis</h1>
-    </div>
-);
+import ProfileSection from '../Components/ProfileSection';
+import './CSS/Analysis.css';
+
+const Analysis = () => {
+    const pageTitle = 'Trend Analysis';
+    const mainContent = (<h1>Main content goes here</h1>);
+    const sideBar = (<h1>Side bar goes here</h1>);
+
+    return (
+        <div className="Analysis">
+            <ProfileSection
+                section="Analysis"
+                pageTitle={pageTitle}
+                mainContent={mainContent}
+                sideBar={sideBar}
+            />
+        </div>
+    );
+};
 
 export default Analysis;
