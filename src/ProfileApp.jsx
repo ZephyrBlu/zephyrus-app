623920f87aa9d75790fdafb46f893c1cd8893fd6
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-12 16:10:19 +1200

    Misc changes

--- src/ProfileApp.jsx
+++ src/ProfileApp.jsx
@@ -1,5 +1,4 @@
 import { Router } from '@reach/router';
-// import { useState } from 'react';
 import Overview from './Tabs/Overview';
 import Replays from './Tabs/Replays';
 import Analysis from './Tabs/Analysis';
