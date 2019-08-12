1406135c10464e5627161cf5a55f0a969075f6e2
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-12 15:37:06 +1200

    Create ProfileSection component
    Created this component to reduce boilerplate and keep consistency in design between pages.'

---
+++ src/Components/ProfileSection.jsx
@@ -0,0 +1,17 @@
+import PageHeader from './PageHeader';
+import './CSS/ProfileSection.css';
+
+const ProfileSection = props => (
+    <div className={`ProfileSection ProfileSection--${props.section}`}>
+        <PageHeader pageTitle={props.pageTitle} />
+        <section className="main-content">
+            {props.mainContent}
+        </section>
+
+        <section className="side-bar">
+            {props.sideBar}
+        </section>
+    </div>
+);
+
+export default ProfileSection;
