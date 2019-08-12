f7fcbe3f749711c2ce67fc9d40ecc94ee47c64df
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-12 15:40:08 +1200

    Refactor Overview section for ProfileSection component
    Overview section is now compatible with the ProfileSection template component.

--- src/Tabs/Overview.jsx
+++ src/Tabs/Overview.jsx
@@ -1,57 +1,64 @@
-import PageHeader from '../Components/PageHeader';
+import { Fragment } from 'react';
+import ProfileSection from '../Components/ProfileSection';
 import StatBlock from '../Components/StatBlock';
 import './CSS/Overview.css';
 
-const Overview = () => (
-    <div className="Overview">
-        <PageHeader pageTitle="Profile Overview" />
-        <section className="summary-stats">
-            <StatBlock
-                title="Recent"
-            />
-            <StatBlock />
-        </section>
-        <section className="analysis-stats">
-            <div className="analysis-stats__stat-type">
-                <div className="analysis-stats__content">
-                    <h1 className="analysis-stats__title">Strengths</h1>
-                    <StatBlock
-                        title="Protoss"
-                        statName="Avg Winrate"
-                    />
-                    <StatBlock
-                        title="Zerg"
-                        statName="Avg Winrate"
-                    />
-                    <StatBlock
-                        title="Terran"
-                        statName="Avg Winrate"
-                    />
-                </div>
-                <div className="analysis-stats__chart">Chart goes here</div>
-            </div>
+const Overview = () => {
+    const pageTitle = 'Profile Overview';
 
-            <div className="analysis-stats__stat-type">
-                <div className="analysis-stats__content">
-                    <h1 className="analysis-stats__title">Weaknesses</h1>
-                    <StatBlock
-                        title="Protoss"
-                        statName="Avg Winrate"
-                    />
-                    <StatBlock
-                        title="Zerg"
-                        statName="Avg Winrate"
-                    />
-                    <StatBlock
-                        title="Terran"
-                        statName="Avg Winrate"
-                    />
-                </div>
-                <div className="analysis-stats__chart">Chart goes here</div>
-            </div>
-        </section>
+    const mainContent = (
+        <Fragment>
+            <section className="summary-stats">
+                <StatBlock
+                    title="Recent"
+                />
+                <StatBlock />
+            </section>
 
-        <section className="map-winrate">
+            <section className="analysis-stats">
+                <div className="analysis-stats__stat-type">
+                    <div className="analysis-stats__content">
+                        <h1 className="analysis-stats__title">Strengths</h1>
+                        <StatBlock
+                            title="Protoss"
+                            statName="Avg Winrate"
+                        />
+                        <StatBlock
+                            title="Zerg"
+                            statName="Avg Winrate"
+                        />
+                        <StatBlock
+                            title="Terran"
+                            statName="Avg Winrate"
+                        />
+                    </div>
+                    <div className="analysis-stats__chart">Chart goes here</div>
+                </div>
+
+                <div className="analysis-stats__stat-type">
+                    <div className="analysis-stats__content">
+                        <h1 className="analysis-stats__title">Weaknesses</h1>
+                        <StatBlock
+                            title="Protoss"
+                            statName="Avg Winrate"
+                        />
+                        <StatBlock
+                            title="Zerg"
+                            statName="Avg Winrate"
+                        />
+                        <StatBlock
+                            title="Terran"
+                            statName="Avg Winrate"
+                        />
+                    </div>
+                    <div className="analysis-stats__chart">Chart goes here</div>
+                </div>
+            </section>
+        </Fragment>
+    );
+
+    const sideBar = (
+        <div className="map-winrate">
             <StatBlock
                 statName="Map Name"
             />
@@ -70,8 +77,19 @@ const Overview = () => (
             <StatBlock
                 statName="Map Name"
             />
-        </section>
-    </div>
-);
+        </div>
+    );
+
+    return (
+        <div className="Overview">
+            <ProfileSection
+                section="Overview"
+                pageTitle={pageTitle}
+                mainContent={mainContent}
+                sideBar={sideBar}
+            />
+        </div>
+    );
+};
 
 export default Overview;
