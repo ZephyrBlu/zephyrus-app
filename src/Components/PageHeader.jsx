299ed359b7db15f941ba5c6c569c9d36baac279e
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-12 14:23:07 +1200

    Refactor boilerplate code into components

---
+++ src/Components/PageHeader.jsx
@@ -0,0 +1,13 @@
+import PageInfo from './PageInfo';
+import ProfileNav from './ProfileNav';
+import './CSS/PageHeader.css';
+
+const PageHeader = props => (
+    <header className="PageHeader">
+        <PageInfo />
+        <h1 className="PageHeader__page-title">{props.pageTitle}</h1>
+        <ProfileNav pages={['Overview', 'Replays', 'Analysis']} />
+    </header>
+);
+
+export default PageHeader;
---
+++ src/Components/PageInfo.jsx
@@ -0,0 +1,12 @@
+import './CSS/PageInfo.css';
+
+const PageInfo = () => (
+    <div className="PageInfo">
+        <h2 className="PageInfo__site-name">zephyrus</h2>
+        <h3 className="PageInfo__info">
+            StarCraft II &nbsp;/&nbsp; Season 39-40 &nbsp;/&nbsp; NA Ladder
+        </h3>
+    </div>
+);
+
+export default PageInfo;
