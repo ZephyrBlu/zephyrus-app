06c912f30f920a289ba741de407943d813ccb545
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-14 17:51:52 +1200

    Refactor app structure to support user login and view restriction
    Renders either the login page or the user's profile based on if an Authentication Token exists in sessionStorage at the time of rendering. Slightly bugged, webpage doesn't redirect on logout yet. Redirect before login works as intended.

--- src/ProfileApp.jsx
+++ src/ProfileApp.jsx
@@ -1,26 +1,61 @@
-import { Router } from '@reach/router';
+import { useState } from 'react';
+import { Router, Redirect } from '@reach/router';
+import LoginForm from './Components/LoginForm';
 import Overview from './Components/Overview/Overview';
 import Replays from './Components/Replays/Replays';
 import Analysis from './Components/Analysis/Analysis';
 import './ProfileApp.css';
 
-const ProfileApp = () => (
-    <div className="ProfileApp">
-        <Router>
-            <Overview
-                pageTitle="Profile Overview"
-                path="/"
-            />
-            <Replays
-                pageTitle="Replays"
-                path="/replays"
-            />
-            <Analysis
-                pageTitle="Trend Analysis"
-                path="/analysis"
-            />
-        </Router>
-    </div>
-);
+const ProfileApp = () => {
+    const [authorizationToken, setAuthorizationToken] = useState(null);
+
+    if (!sessionStorage.token && authorizationToken) {
+        sessionStorage.token = authorizationToken;
+    }
+
+    const updateToken = (newToken) => {
+        setAuthorizationToken(newToken);
+    };
+
+    const currentToken = sessionStorage.token;
+
+    let app;
+    if (currentToken) {
+        app = (
+            <Router>
+                <Redirect from="/login" to="/" />
+                <Overview
+                    pageTitle="Profile Overview"
+                    path="/"
+                />
+                <Replays
+                    pageTitle="Replays"
+                    path="/replays"
+                />
+                <Analysis
+                    pageTitle="Trend Analysis"
+                    path="/analysis"
+                />
+            </Router>
+        );
+    } else {
+        app = (
+            <Router>
+                <Redirect from="/*" to="/login" />
+                <LoginForm
+                    authToken={currentToken}
+                    handleToken={updateToken}
+                    path="/login"
+                />
+            </Router>
+        );
+    }
+
+    return (
+        <div className="ProfileApp">
+            {app}
+        </div>
+    );
+};
 
 export default ProfileApp;
