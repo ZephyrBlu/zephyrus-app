43ce2983baef5febc1e4c9801a7362ebe04c87f0
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-18 15:23:59 +1200

    Remove logout from ProfileNav and add to ProfileSection

--- src/Components/General/ProfileNav.jsx
+++ src/Components/General/ProfileNav.jsx
@@ -1,37 +1,13 @@
-import { useSelector, useDispatch } from 'react-redux';
 import { Link } from '@reach/router';
-import { setAuthToken } from '../../actions';
 import './CSS/ProfileNav.css';
 
 const ProfileNav = (props) => {
-    const dispatch = useDispatch();
-    const token = useSelector(state => `Token ${state.token}`);
-
     const isActive = ({ isCurrent }) => (
         isCurrent ?
             { className: 'ProfileNav__link ProfileNav__link--active' }
             :
             { className: 'ProfileNav__link' }
     );
-
-    const handleLogout = async () => {
-        const url = 'http://127.0.0.1:8000/api/logout/';
-
-        const error = await fetch(url, {
-            method: 'GET',
-            headers: {
-                Authorization: token,
-            },
-        }).then(response => (
-            response.status === 200 ? null : `${response.status} ${response.statusText}`
-        ));
-
-        if (error) {
-            alert('Something went wrong. Please try to logout again');
-        }
-        sessionStorage.clear();
-        dispatch(setAuthToken(null));
-    };
 
     return (
         <nav className={`ProfileNav ProfileNav--${props.pages[0]}`}>
@@ -50,9 +26,6 @@ const ProfileNav = (props) => {
                     {pageName}
                 </Link>
             ))}
-            <button onClick={handleLogout}>
-                Logout
-            </button>
         </nav>
     );
 };
--- src/Components/General/ProfileSection.jsx
+++ src/Components/General/ProfileSection.jsx
@@ -1,19 +1,50 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { setAuthToken } from '../../actions';
 import PageHeader from './PageHeader';
 import './CSS/ProfileSection.css';
 
-const ProfileSection = props => (
-    <div className={`ProfileSection ProfileSection--${props.section}`}>
-        <PageHeader
-            pageTitle={props.pageTitle}
-        />
-        <section className="main-content">
-            {props.mainContent}
-        </section>
+const ProfileSection = (props) => {
+    const dispatch = useDispatch();
+    const token = useSelector(state => `Token ${state.token}`);
 
-        <section className="side-bar">
-            {props.sideBar}
-        </section>
-    </div>
-);
+    const handleLogout = async () => {
+        const url = 'http://127.0.0.1:8000/api/logout/';
+
+        const error = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: token,
+            },
+        }).then(response => (
+            response.status === 200 ? null : `${response.status} ${response.statusText}`
+        ));
+
+        if (error) {
+            alert('Something went wrong. Please try to logout again');
+        }
+        sessionStorage.clear();
+        dispatch(setAuthToken(null));
+    };
+
+    return (
+        <div className={`ProfileSection ProfileSection--${props.section}`}>
+            <PageHeader
+                pageTitle={props.pageTitle}
+            />
+            <section className="main-content">
+                {props.mainContent}
+            </section>
+
+            <section className="side-bar">
+                <div className="side-bar__content">
+                    {props.sideBar}
+                </div>
+                <button className="side-bar__logout" onClick={handleLogout}>
+                    Logout
+                </button>
+            </section>
+        </div>
+    );
+};
 
 export default ProfileSection;
