871e0e4a8a41222a8f88f83a1049174f86ea34e0
ZephyrBlu <lukejholroyd@gmail.com>
2019-09-17 06:04:58 +1200

    Add custom Tooltips for chart and info icons

---
+++ src/Components/General/Tooltip.jsx
@@ -0,0 +1,111 @@
+import { Fragment } from 'react';
+import './CSS/Tooltip.css';
+
+const CustomTooltip = (props) => {
+    let content = (
+        <p className="Tooltip__default">Loading...</p>
+    );
+
+    const statOrder = {
+        winrate: 'Winrate',
+        sq: 'SQ',
+        apm: 'APM',
+        avg_pac_action_latency: 'PAC Action Latency',
+        avg_pac_actions: 'PAC Actions',
+        avg_pac_gap: 'PAC Gap',
+        avg_pac_per_min: 'PAC Per Minute',
+        workers_produced: 'Workers Produced',
+        workers_lost: 'Workers Lost',
+        avg_unspent_resources_minerals: 'Unspent Minerals',
+        avg_unspent_resources_gas: 'Unspent Gas',
+        avg_resource_collection_rate_minerals: 'Mineral Collection Rate',
+        avg_resource_collection_rate_gas: 'Gas Collection Rate',
+        resources_lost_minerals: 'Minerals Lost',
+        resources_lost_gas: 'Gas Lost',
+        inject_count: 'Inject Count',
+    };
+
+    const colours = {
+        winrate: 'white',
+        sq: 'red',
+        apm: 'hsl(210, 68%, 47%)',
+        avg_pac_action_latency: 'gold',
+        avg_pac_actions: 'orange',
+        avg_pac_gap: 'violet',
+        avg_pac_per_min: 'cyan',
+        workers_produced: 'green',
+        workers_lost: 'purple',
+        avg_unspent_resources_minerals: '#00FF7F',
+        avg_unspent_resources_gas: 'brown',
+        avg_resource_collection_rate_minerals: '#014421',
+        avg_resource_collection_rate_gas: '#36454F',
+        resources_lost_minerals: '#8B008B',
+        resources_lost_gas: '#FBEC5D',
+        inject_count: 'grey',
+    };
+
+    if (props.payload !== undefined && props.payload.length !== 0) {
+        switch (props.chart) {
+            case 'analysis':
+                content = (
+                    <Fragment>
+                        <ul className="Tooltip__content">
+                            <li className="Tooltip__title">
+                                Week Starting&nbsp;{props.label}
+                            </li>
+                            <li className="Tooltip__games">
+                                {props.payload[0].payload.count < 5 ?
+                                    '<5' : props.payload[0].payload.count} Games
+                            </li>
+                            {props.payload.map((payload, index) => {
+                                const key = props.payload[index].dataKey.slice(0, -3);
+
+                                return (
+                                    props.lineState[payload.name.slice(0, -3)] === 0 ?
+                                        null
+                                        :
+                                        <li className="Tooltip__stat">
+                                            <svg height="10" width="10">
+                                                <circle cx="5" cy="5" r="5" fill={colours[key]} />
+                                            </svg>&nbsp;
+                                            <span>
+                                                {statOrder[payload.name.slice(0, -3)]}
+                                            </span>&nbsp;
+                                            {payload.name.slice(0, -3) === 'winrate' ?
+                                                `${payload.payload[key][0]}%`
+                                                :
+                                                payload.payload[key][0]} (
+                                            <span
+                                                className={
+                                                    `Tooltip__value ${payload.payload[key][1] > 0 ?
+                                                        `Tooltip__value--positive Tooltip__value--${key}-positive`
+                                                        :
+                                                        `Tooltip__value--negative Tooltip__value--${key}-negative`}`
+                                                }
+                                            >
+                                                {payload.payload[key][1] > 0 ?
+                                                    `+${payload.payload[key][1]}`
+                                                    :
+                                                    payload.payload[key][1]}%
+                                            </span>)
+                                        </li>
+                                );
+                            })}
+                        </ul>
+                    </Fragment>
+                );
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    return (
+        <div id="tooltip">
+            {content}
+        </div>
+    );
+};
+
+export default CustomTooltip;
