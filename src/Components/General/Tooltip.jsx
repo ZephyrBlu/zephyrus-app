d156d3f8f02199627e4f19b649275acc8e13ad96
ZephyrBlu <lukejholroyd@gmail.com>
2020-03-29 16:08:49 +1300

    Add more stats + 3mo period support + new line colour scheme

--- src/Components/General/Tooltip.jsx
+++ src/Components/General/Tooltip.jsx
@@ -6,40 +6,44 @@ const CustomTooltip = (props) => {
 
     const statOrder = {
         winrate: 'Winrate',
+        mmr: 'MMR',
         sq: 'SQ',
         apm: 'APM',
-        avg_pac_action_latency: 'PAC Action Latency',
-        avg_pac_actions: 'PAC Actions',
-        avg_pac_gap: 'PAC Gap',
-        avg_pac_per_min: 'PAC Per Minute',
+        // avg_pac_action_latency: 'PAC Action Latency',
+        // avg_pac_actions: 'PAC Actions',
+        // avg_pac_gap: 'PAC Gap',
+        // avg_pac_per_min: 'PAC Per Minute',
         workers_produced: 'Workers Produced',
+        workers_killed: 'Workers Killed',
         workers_lost: 'Workers Lost',
         avg_unspent_resources_minerals: 'Unspent Minerals',
         avg_unspent_resources_gas: 'Unspent Gas',
-        avg_resource_collection_rate_minerals: 'Mineral Collection Rate',
-        avg_resource_collection_rate_gas: 'Gas Collection Rate',
-        resources_lost_minerals: 'Minerals Lost',
-        resources_lost_gas: 'Gas Lost',
+        // avg_resource_collection_rate_minerals: 'Mineral Collection Rate',
+        // avg_resource_collection_rate_gas: 'Gas Collection Rate',
+        // resources_lost_minerals: 'Minerals Lost',
+        // resources_lost_gas: 'Gas Lost',
         inject_count: 'Inject Count',
     };
 
     const colours = {
         winrate: 'white',
-        sq: 'red',
-        apm: 'hsl(210, 68%, 47%)',
-        avg_pac_action_latency: 'gold',
-        avg_pac_actions: 'orange',
-        avg_pac_gap: 'violet',
-        avg_pac_per_min: 'cyan',
-        workers_produced: 'green',
-        workers_lost: 'purple',
-        avg_unspent_resources_minerals: '#00FF7F',
-        avg_unspent_resources_gas: 'brown',
-        avg_resource_collection_rate_minerals: '#014421',
-        avg_resource_collection_rate_gas: '#36454F',
-        resources_lost_minerals: '#8B008B',
-        resources_lost_gas: '#FBEC5D',
-        inject_count: 'grey',
+        mmr: 'var(--line-shade-1)',
+        sq: 'var(--line-shade-2)',
+        apm: 'var(--line-shade-3)',
+        // avg_pac_action_latency: 'gold',
+        // avg_pac_actions: 'orange',
+        // avg_pac_gap: 'violet',
+        // avg_pac_per_min: 'cyan',
+        workers_produced: 'var(--line-shade-4)',
+        workers_killed: 'var(--line-shade-5)',
+        workers_lost: 'var(--line-shade-6)',
+        avg_unspent_resources_minerals: 'var(--line-shade-7)',
+        avg_unspent_resources_gas: 'var(--line-shade-8)',
+        // avg_resource_collection_rate_minerals: '#014421',
+        // avg_resource_collection_rate_gas: '#36454F',
+        // resources_lost_minerals: '#8B008B',
+        // resources_lost_gas: '#FBEC5D',
+        inject_count: 'var(--line-shade-9)',
     };
 
     if (props.payload !== undefined && props.payload.length !== 0) {
@@ -52,8 +56,8 @@ const CustomTooltip = (props) => {
                                 {props.tickFormatter(props.label, 'chart')}
                             </li>
                             <li className="Tooltip__games">
-                                {props.payload[0].payload.count < 5 ?
-                                    '<5' : props.payload[0].payload.count} Games
+                                {props.payload[0].payload.count}&nbsp;/&nbsp;
+                                {props.payload[0].payload.total_count} Games
                             </li>
                             {props.payload.map((payload, index) => {
                                 const key = props.payload[index].dataKey.slice(0, -3);
@@ -90,6 +94,13 @@ const CustomTooltip = (props) => {
                                         </li>
                                 );
                             })}
+                            <li className="Tooltip__games" style={{ margin: '10px 0 0', textAlign: 'center' }}>
+                                3-Month Period:&nbsp;
+                                {props.tickFormatter(props.payload[0].payload.start_date, 'period')}
+                                -
+                                {props.tickFormatter(props.payload[0].payload.end_date, 'period')}
+                                &nbsp;mo
+                            </li>
                         </ul>
                     </Fragment>
                 );
