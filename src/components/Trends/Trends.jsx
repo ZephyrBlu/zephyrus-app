285e3876644e599eef90cf1d779304e1dd7a1854
ZephyrBlu <lukejholroyd@gmail.com>
2020-08-01 17:52:39 +1200

    Add handling for edge case
    If there were no replays for one matchup, the application would crash if the no-replay matchup was selected

--- src/components/Trends/Trends.jsx
+++ src/components/Trends/Trends.jsx
@@ -44,7 +44,7 @@ const Trends = () => {
         let previousSeasonTrends = null;
         let mmrTrends = null;
 
-        if (!trends) {
+        if (!trends || !trends[trendsMatchup]) {
             return { mmrTrends, currentSeasonTrends, previousSeasonTrends };
         }
 
@@ -130,35 +130,36 @@ const Trends = () => {
 
                 return (
                     <Fragment>
-                        <div className="Trends__title-stat">
-                            <span className="Trends__title-text">
-                                {_currentSeasonTrends ? 'Currently at' : 'Finished at'}
-                            </span>
-                            <div className="Trends__title-value">
-                                {selectTrends('mmr').mmr.end}
-                            </div>
-                            <span className="Trends__title-text">
-                                MMR
-                                {` (${selectTrends('mmr').mmr.values.slice(-1)[0].value - selectTrends('mmr').mmr.values[0].value >= 0 ? '+' : ''}${selectTrends('mmr').mmr.values.slice(-1)[0].value - selectTrends('mmr').mmr.values[0].value} this season)`}
-                                &nbsp;with a
-                            </span>
-                            <div className="Trends__title-value">
-                                {selectTrends().winrate}%
-                            </div>
-                            <span className="Trends__title-text">
-                                winrate over&nbsp;
-                                {selectTrends().count}
-                            </span>
-                            {_trendsMatchup !== 'all' &&
-                                <Fragment>
-                                    <div className="Trends__title-value">
-                                        {`${_selectedRace.charAt(0).toUpperCase()}v${_trendsMatchup.charAt(0).toUpperCase()}`}
-                                    </div>
-                                </Fragment>}
-                            <span className="Trends__title-text">
-                                {_trendsMatchup === 'all' && ' '}games
-                            </span>
-                        </div>
+                        {(_currentSeasonTrends || _previousSeasonTrends) &&
+                            <div className="Trends__title-stat">
+                                <span className="Trends__title-text">
+                                    {_currentSeasonTrends ? 'Currently at' : 'Finished at'}
+                                </span>
+                                <div className="Trends__title-value">
+                                    {selectTrends('mmr').mmr.end}
+                                </div>
+                                <span className="Trends__title-text">
+                                    MMR
+                                    {` (${selectTrends('mmr').mmr.values.slice(-1)[0].value - selectTrends('mmr').mmr.values[0].value >= 0 ? '+' : ''}${selectTrends('mmr').mmr.values.slice(-1)[0].value - selectTrends('mmr').mmr.values[0].value} this season)`}
+                                    &nbsp;with a
+                                </span>
+                                <div className="Trends__title-value">
+                                    {selectTrends().winrate}%
+                                </div>
+                                <span className="Trends__title-text">
+                                    winrate over&nbsp;
+                                    {selectTrends().count}
+                                </span>
+                                {_trendsMatchup !== 'all' &&
+                                    <Fragment>
+                                        <div className="Trends__title-value">
+                                            {`${_selectedRace.charAt(0).toUpperCase()}v${_trendsMatchup.charAt(0).toUpperCase()}`}
+                                        </div>
+                                    </Fragment>}
+                                <span className="Trends__title-text">
+                                    {_trendsMatchup === 'all' && ' '}games
+                                </span>
+                            </div>}
                         <div className="Trends__season-stat-controls Trends__season-stat-controls--global">
                             <span className="Trends__season-stat-options-wrapper">
                                 {Object.entries(statControls.type).map(([controlKey, controlText]) => (
@@ -197,99 +198,104 @@ const Trends = () => {
                                 ))}
                             </span>
                         </div>
-                        <div className="Trends__season-stats">
-                            {generalStats.map(stat => (
-                                <div className={`Trends__season-stat-wrapper Trends__season-stat-wrapper--${stat}`}>
-                                    <div className="Trends__season-stat">
-                                        <div className="Trends__season-stat-data">
-                                            <h2 className="Trends__season-stat-name">
-                                                {statNames[stat]}
-                                                <InfoTooltip
-                                                    content={statDescriptions[stat]}
-                                                    style={{ top: 0 }}
-                                                />
-                                            </h2>
-                                            <p className="Trends__season-stat-value">
-                                                Med: {selectTrends()[stat].avg}
-                                                {_currentSeasonTrends && _previousSeasonTrends && ` (${calcStatDiff(stat)})`}
-                                            </p>
+                        {(_currentSeasonTrends || _previousSeasonTrends) &&
+                            <div className="Trends__season-stats">
+                                {generalStats.map(stat => (
+                                    <div className={`Trends__season-stat-wrapper Trends__season-stat-wrapper--${stat}`}>
+                                        <div className="Trends__season-stat">
+                                            <div className="Trends__season-stat-data">
+                                                <h2 className="Trends__season-stat-name">
+                                                    {statNames[stat]}
+                                                    <InfoTooltip
+                                                        content={statDescriptions[stat]}
+                                                        style={{ top: 0 }}
+                                                    />
+                                                </h2>
+                                                <p className="Trends__season-stat-value">
+                                                    Med: {selectTrends()[stat].avg}
+                                                    {_currentSeasonTrends && _previousSeasonTrends && ` (${calcStatDiff(stat)})`}
+                                                </p>
+                                            </div>
+                                            {stat !== 'mmr' &&
+                                                <div className="Trends__season-stat-controls">
+                                                    {Object.entries(statControls.type).map(([controlKey, controlText]) => (
+                                                        <button
+                                                            className={`
+                                                                Trends__season-stat-option
+                                                                Trends__season-stat-option--stat
+                                                                Trends__season-stat-option--type
+                                                                Trends__season-stat-option--${controlKey}
+                                                                ${_trendOptions[stat] === controlKey ? 'Trends__season-stat-option--active' : ''}
+                                                            `}
+                                                            onClick={() => _setTrendOptions(prevState => ({
+                                                                ...prevState,
+                                                                [stat]: controlKey,
+                                                            }))}
+                                                        >
+                                                            {controlText}
+                                                        </button>
+                                                    ))}
+                                                </div>}
                                         </div>
-                                        {stat !== 'mmr' &&
-                                            <div className="Trends__season-stat-controls">
-                                                {Object.entries(statControls.type).map(([controlKey, controlText]) => (
-                                                    <button
-                                                        className={`
-                                                            Trends__season-stat-option
-                                                            Trends__season-stat-option--stat
-                                                            Trends__season-stat-option--type
-                                                            Trends__season-stat-option--${controlKey}
-                                                            ${_trendOptions[stat] === controlKey ? 'Trends__season-stat-option--active' : ''}
-                                                        `}
-                                                        onClick={() => _setTrendOptions(prevState => ({
-                                                            ...prevState,
-                                                            [stat]: controlKey,
-                                                        }))}
-                                                    >
-                                                        {controlText}
-                                                    </button>
-                                                ))}
-                                            </div>}
-                                    </div>
-                                    <ResponsiveContainer width="99%" height={125}>
-                                        {stat === 'mmr' ?
-                                            <LineChart
-                                                data={selectTrends('mmr')[stat].values}
-                                                className="Trends__season-stat-chart"
-                                                margin={{ left: -15, right: 2, top: 10, bottom: 10 }}
-                                            >
-                                                <YAxis type="number" domain={['auto', 'auto']} interval={0} />
-                                                <ReferenceLine y={selectTrends('mmr')[stat].values[0].value} stroke="hsl(0, 0%, 47%)" strokeWidth={0.5} />
-                                                <Line
-                                                    type="monotone"
-                                                    dataKey="value"
-                                                    stroke="hsl(210, 68%, 47%)"
-                                                    strokeWidth={2}
-                                                    dot={false}
-                                                />
-                                            </LineChart>
-                                            :
-                                            <BarChart
-                                                data={selectTrends()[stat].values[_trendOptions[stat]]}
-                                                className="Trends__season-stat-chart"
-                                                margin={{ bottom: -10 }}
-                                            >
-                                                <XAxis
-                                                    dataKey="bin"
-                                                    interval={0}
-                                                    style={stat === 'match_length' ? { fontSize: '14px' } : {}}
-                                                />
-                                                {_trendOptions[stat] === 'all' &&
-                                                    <Bar
+                                        <ResponsiveContainer width="99%" height={125}>
+                                            {stat === 'mmr' ?
+                                                <LineChart
+                                                    data={selectTrends('mmr')[stat].values}
+                                                    className="Trends__season-stat-chart"
+                                                    margin={{ left: -15, right: 2, top: 10, bottom: 10 }}
+                                                >
+                                                    <YAxis type="number" domain={['auto', 'auto']} interval={0} />
+                                                    <ReferenceLine y={selectTrends('mmr')[stat].values[0].value} stroke="hsl(0, 0%, 47%)" strokeWidth={0.5} />
+                                                    <Line
                                                         type="monotone"
                                                         dataKey="value"
-                                                        fill="hsl(210, 68%, 47%)"
-                                                        radius={[8, 8, 0, 0]}
-                                                    />}
-                                                {_trendOptions[stat] === 'win_loss' &&
-                                                    <Bar
-                                                        type="monotone"
-                                                        dataKey="win"
-                                                        stackId="wl"
-                                                        fill="hsla(120, 80%, 25%, 0.9)"
-                                                    />}
-                                                {_trendOptions[stat] === 'win_loss' &&
-                                                    <Bar
-                                                        type="monotone"
-                                                        dataKey="loss"
-                                                        stackId="wl"
-                                                        fill="hsla(0, 70%, 25%, 0.9)"
-                                                        radius={[8, 8, 0, 0]}
-                                                    />}
-                                            </BarChart>}
-                                    </ResponsiveContainer>
-                                </div>
-                            ))}
-                        </div>
+                                                        stroke="hsl(210, 68%, 47%)"
+                                                        strokeWidth={2}
+                                                        dot={false}
+                                                    />
+                                                </LineChart>
+                                                :
+                                                <BarChart
+                                                    data={selectTrends()[stat].values[_trendOptions[stat]]}
+                                                    className="Trends__season-stat-chart"
+                                                    margin={{ bottom: -10 }}
+                                                >
+                                                    <XAxis
+                                                        dataKey="bin"
+                                                        interval={0}
+                                                        style={stat === 'match_length' ? { fontSize: '14px' } : {}}
+                                                    />
+                                                    {_trendOptions[stat] === 'all' &&
+                                                        <Bar
+                                                            type="monotone"
+                                                            dataKey="value"
+                                                            fill="hsl(210, 68%, 47%)"
+                                                            radius={[8, 8, 0, 0]}
+                                                        />}
+                                                    {_trendOptions[stat] === 'win_loss' &&
+                                                        <Bar
+                                                            type="monotone"
+                                                            dataKey="win"
+                                                            stackId="wl"
+                                                            fill="hsla(120, 80%, 25%, 0.9)"
+                                                        />}
+                                                    {_trendOptions[stat] === 'win_loss' &&
+                                                        <Bar
+                                                            type="monotone"
+                                                            dataKey="loss"
+                                                            stackId="wl"
+                                                            fill="hsla(0, 70%, 25%, 0.9)"
+                                                            radius={[8, 8, 0, 0]}
+                                                        />}
+                                                </BarChart>}
+                                        </ResponsiveContainer>
+                                    </div>
+                                ))}
+                            </div>}
+                        {!(_currentSeasonTrends || _previousSeasonTrends) &&
+                            <div className="Trends__season-stats Trends__season-stats--default">
+                                No {`${_selectedRace.charAt(0).toUpperCase()}v${_trendsMatchup.charAt(0).toUpperCase()}`} replays found
+                            </div>}
                     </Fragment>
                 );
             },
