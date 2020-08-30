8fb95def284e199f9418ccbf2965458fbdf6debd
ZephyrBlu <lukejholroyd@gmail.com>
2020-08-30 18:09:58 +1200

    Restore removed Replays and TimelineArea files

---
+++ src/components/Replays/Replays.jsx
@@ -0,0 +1,308 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+import { useState, useEffect, useContext } from 'react';
+import { setReplayInfo } from '../../actions';
+import { useFetch, useAuthCode, useLoadingState } from '../../hooks';
+import UrlContext from '../../index';
+import ReplayView from './ReplayView';
+import ReplayList from './ReplayList';
+import DefaultResponse from '../shared/DefaultResponse';
+import LoadingAnimation from '../shared/LoadingAnimation';
+import './CSS/Replays.css';
+
+const selectData = createSelector(
+    state => state.user,
+    state => state.replayInfo,
+    state => state.selectedReplayHash,
+    (user, replayInfo, selectedReplayHash) => (
+        [user, replayInfo, selectedReplayHash]
+    ),
+);
+
+const Replays = ({ visibleState }) => {
+    useAuthCode();
+    const dispatch = useDispatch();
+    const urlPrefix = useContext(UrlContext);
+    const [replayListState, setReplayListState] = useState({ loadingState: 'IN_PROGRESS' });
+    const [selectedReplayState, setSelectedReplayState] = useState({
+        data: {
+            data: null,
+            info: null,
+        },
+        loadingState: 'INITIAL',
+    });
+    const [timelineState, setTimelineState] = useState({
+        data: null,
+        loadingState: 'INITIAL',
+    });
+
+    if (!localStorage.timelineStat) {
+        localStorage.timelineStat = 'resource_collection_rate_all';
+    }
+
+    const [timelineStat, setTimelineStat] = useState(localStorage.timelineStat);
+    const [user, replayInfo, selectedReplayHash] = useSelector(selectData);
+    const [currentGameloop, setCurrentGameloop] = useState(0);
+    const [metrics, setMetrics] = useState(null);
+
+    useEffect(() => {
+        if (!timelineState.data) {
+            return;
+        }
+
+        const gameMetrics = ['resource_collection_rate_all', 'total_army_value', 'total_resources_lost'];
+        const med = (arr) => {
+            const sep1 = [];
+            const sep2 = [];
+
+            arr.forEach(([t, p]) => {
+                sep1.push(t);
+                sep2.push(p);
+            });
+
+            const data = sep2.map((val) => {
+                let limitedVal = Number(val.toFixed(2));
+
+                if (limitedVal > 1) {
+                    limitedVal = 1;
+                } else if (limitedVal < -1) {
+                    limitedVal = -1;
+                }
+
+                return { value: limitedVal };
+            });
+
+            sep1.sort((a, b) => (
+                a - b
+            ));
+
+            sep2.sort((a, b) => (
+                a - b
+            ));
+
+            const medians = [];
+
+            [sep1, sep2].forEach((a, i) => {
+                const half = Math.floor(a.length / 2);
+
+                if (a.length % 2) {
+                    medians.push(i === 0 ? Math.round(a[half]) : Math.round((a[half] * 100)));
+                } else {
+                    medians.push(i === 0 ? Math.round((a[half - 1] + a[half]) / 2) : Math.round((((a[half - 1] + a[half]) * 100) / 2)));
+                }
+            });
+
+            return {
+                medians,
+                data,
+            };
+        };
+        const currentMetrics = {};
+
+        gameMetrics.forEach((metric) => {
+            let ahead = 0;
+            let behind = 0;
+            const amountAhead = [];
+            const amountBehind = [];
+            const leadLag = [];
+
+            timelineState.data.data.forEach((gameState) => {
+                const userId = selectedReplayState.data.info.user_match_id;
+                const oppId = userId === 1 ? 2 : 1;
+
+                const userVal = gameState[userId][metric];
+                const oppVal = gameState[oppId][metric];
+
+                let diff;
+                if (userVal === 0 && oppVal === 0) {
+                    diff = [0, 0];
+                } else {
+                    diff = [
+                        userVal - oppVal,
+                        ((Math.min(userVal, oppVal) === 0 ?
+                            0 : Math.max(userVal, oppVal) / Math.min(userVal, oppVal)) - 1)
+                            * (Math.max(userVal, oppVal) === userVal ? 1 : -1),
+                    ];
+                }
+                if (userVal > oppVal) {
+                    ahead += 1;
+                    amountAhead.push(diff);
+                } else if (userVal < oppVal) {
+                    behind += 1;
+                    amountBehind.push(diff.map(val => val * -1));
+                }
+                leadLag.push(diff);
+            });
+
+            const leadLagData = med(leadLag);
+            currentMetrics[metric] = {
+                data: leadLagData.data,
+                summary: {
+                    ahead: Math.round(((ahead / timelineState.data.data.length) * 100)),
+                    behind: Math.round(((behind / timelineState.data.data.length) * 100)),
+                    avgAhead: med(amountAhead).medians,
+                    avgBehind: med(amountBehind).medians,
+                    avgLeadLag: leadLagData.medians,
+                },
+            };
+        });
+
+        setMetrics(currentMetrics);
+    }, [timelineState.data]);
+
+    const userReplays = useSelector(state => (
+        state.selectedRace
+            ? state.raceData[state.selectedRace].replays
+            : null
+    ));
+
+    const dataStates = {
+        replayList: {
+            IN_PROGRESS: (<LoadingAnimation />),
+            SUCCESS: data => (<ReplayList replays={data} />),
+            NOT_FOUND: (<DefaultResponse content="We couldn't find any replays" />),
+            ERROR: (<DefaultResponse content="Something went wrong" />),
+        },
+    };
+
+    const clanTagIndex = name => (
+        name.indexOf('>') === -1 ? 0 : name.indexOf('>') + 1
+    );
+
+    useEffect(() => {
+        const filterReplayInfo = () => {
+            const newReplays = [];
+            userReplays.forEach((replay) => {
+                const currentReplayInfo = {
+                    fileHash: replay.file_hash,
+                    matchup: `${replay.players[1].race.slice(0, 1)}v${replay.players[2].race.slice(0, 1)}`,
+                    map: replay.map,
+                    result: `${replay.win ? 'Win' : 'Loss'},\xa0\xa0\xa0\xa0${Math.ceil(replay.match_length / 60)}`,
+                    date: replay.played_at.slice(0, 10),
+                    player1: `${replay.players[1].name.slice(clanTagIndex(replay.players[1].name))},
+                    ${replay.match_data.mmr[1] === 0 ? '' : replay.match_data.mmr[1]}`,
+                    player2: `${replay.players[2].name.slice(clanTagIndex(replay.players[2].name))},
+                    ${replay.match_data.mmr[2] === 0 ? '' : replay.match_data.mmr[2]}`,
+                };
+                newReplays.push(currentReplayInfo);
+            });
+            dispatch(setReplayInfo(newReplays));
+        };
+
+        if (userReplays) {
+            filterReplayInfo();
+            setReplayListState({ loadingState: userReplays.length > 0 ? 'SUCCESS' : 'NOT_FOUND' });
+        } else if (userReplays === false) {
+            setReplayListState({ loadingState: 'ERROR' });
+        }
+    }, [userReplays]);
+
+    const url = `${urlPrefix}api/replays/timeline/${selectedReplayHash}/`;
+    const timelineUrl = useFetch(url, selectedReplayHash, 'timeline_url', {
+        method: 'GET',
+        headers: {
+            Authorization: `Token ${user.token}`,
+            'Accept-Encoding': 'gzip',
+        },
+    });
+    const replayTimeline = useFetch(timelineUrl, 'default', 'timeline', { method: 'GET' });
+
+    useEffect(() => {
+        if (replayTimeline) {
+            const timeline = {};
+            replayTimeline.forEach((gamestate) => {
+                timeline[gamestate[1].gameloop] = gamestate;
+            });
+
+            setTimelineState({
+                data: {
+                    data: replayTimeline,
+                    cached: timeline,
+                },
+                loadingState: 'SUCCESS',
+            });
+        } else if (replayTimeline === false) {
+            setTimelineState(prevState => ({
+                ...prevState,
+                loadingState: 'ERROR',
+            }));
+        }
+    }, [replayTimeline]);
+
+    useEffect(() => {
+        setTimelineState({
+            data: null,
+            loadingState: 'INITIAL',
+        });
+
+        const getSelectedReplay = () => {
+            userReplays.forEach((replay) => {
+                if (replay.file_hash === selectedReplayHash) {
+                    const infoList = { user_match_id: replay.user_match_id };
+                    Object.entries(replay.match_data).forEach(([stat, values]) => {
+                        infoList[stat] = values;
+                    });
+                    setSelectedReplayState({
+                        data: {
+                            data: replay,
+                            info: infoList,
+                        },
+                        loadingState: 'SUCCESS',
+                    });
+                }
+            });
+        };
+
+        if (selectedReplayHash) {
+            setTimelineState({
+                data: null,
+                loadingState: 'IN_PROGRESS',
+            });
+        }
+
+        if (userReplays) {
+            getSelectedReplay();
+        }
+    }, [selectedReplayHash]);
+
+    const replayListData = {
+        data: replayInfo,
+        ...replayListState,
+    };
+    const ReplayListState = useLoadingState(replayListData, dataStates.replayList);
+
+    return (
+        <div
+            className="Replays"
+            style={visibleState ? {} : { gridTemplateColumns: '1fr 0px' }}
+        >
+            <div className="Replays__main-content">
+                <ReplayView
+                    replay={{
+                        ...selectedReplayState.data,
+                        loading: selectedReplayState.loadingState,
+                        metrics,
+                        hash: selectedReplayHash,
+                    }}
+                    timeline={{
+                        ...timelineState.data,
+                        loading: timelineState.loadingState,
+                        stat: timelineStat,
+                        setStat: setTimelineStat,
+                    }}
+                    gameloop={{
+                        current: currentGameloop,
+                        set: setCurrentGameloop,
+                    }}
+                    clanTagIndex={clanTagIndex}
+                    visibleState={visibleState}
+                />
+            </div>
+            <div className="Replays__sidebar">
+                <ReplayListState />
+            </div>
+        </div>
+    );
+};
+
+export default Replays;
---
+++ src/components/Replays/TimelineArea.jsx
@@ -0,0 +1,264 @@
+import {
+    ResponsiveContainer,
+    LineChart,
+    XAxis,
+    YAxis,
+    Tooltip,
+    Line,
+    ReferenceLine,
+} from 'recharts';
+import { Fragment, useState } from 'react';
+import RaceState from './RaceState';
+import ObjectState from './ObjectState';
+// import CurrentSelectionState from './CurrentSelectionState';
+import UpgradeState from './UpgradeState';
+import TimelineTooltip from './TimelineTooltip';
+import './CSS/TimelineArea.css';
+
+const TimelineArea = ({ metrics, timeline, gameloop, players, visibleState }) => {
+    const [isTimelineFrozen, setTimelineState] = useState(false);
+    const currentTimelineState = timeline.cached[gameloop.current];
+
+    const formatTick = (content) => {
+        const totalSeconds = Math.floor(Number(content) / 22.4);
+        const minutes = Math.floor(totalSeconds / 60);
+        const seconds = totalSeconds - (minutes * 60);
+        if (String(seconds).length === 1) {
+            return `${minutes}:0${seconds}`;
+        }
+        return `${minutes}:${seconds}`;
+    };
+
+    const formatCurrentTime = (tickGameloop) => {
+        const totalSeconds = Math.floor(tickGameloop / 22.4);
+        const minutes = Math.floor(totalSeconds / 60);
+        const seconds = totalSeconds - (minutes * 60);
+        return String(seconds).length === 1 ?
+            `${minutes}:0${seconds}`
+            :
+            `${minutes}:${seconds}`;
+    };
+
+    const timelineStatCategories = {
+        resource_collection_rate_all: ['Collection Rate', 'resource_collection_rate'],
+        // 'unspent_resources.minerals': 'Unspent Minerals',
+        // 'unspent_resources.gas': 'Unspent Gas',
+        total_army_value: ['Army Value', 'army_value'],
+        total_resources_lost: ['Resources Lost', 'resources_lost'],
+        // 'resources_lost.minerals': 'Minerals Lost',
+        // 'resources_lost.gas': 'Gas Lost',
+        // workers_active: 'Workers Active',
+        // workers_killed: 'Workers Lost',
+    };
+
+    const objectStates = {
+        unit: ['live', 'died'],
+        building: ['live', 'died', 'in_progress'],
+    };
+
+    const ignoreObjects = {
+        unit: ['BroodlingEscort'],
+        building: ['CreepTumor', 'CreepTumorQueen'],
+    };
+
+    return (
+        <div className="TimelineArea">
+            <div className="TimelineArea__chart-area">
+                <ResponsiveContainer className="TimelineArea__timeline" width="99%" height={100}>
+                    <LineChart
+                        data={timeline.data}
+                        margin={{ left: 30, right: 20 }}
+                        onClick={
+                            () => (
+                                isTimelineFrozen ?
+                                    setTimelineState(false) : setTimelineState(true)
+                            )
+                        }
+                    >
+                        <XAxis
+                            dataKey="1.gameloop"
+                            tickFormatter={content => formatTick(content)}
+                            hide
+                        />
+                        <Tooltip
+                            content={
+                                <TimelineTooltip
+                                    timeline={{
+                                        stat: timeline.stat,
+                                        state: currentTimelineState,
+                                        frozen: isTimelineFrozen,
+                                    }}
+                                    gameloop={gameloop}
+                                    players={players}
+                                />
+                            }
+                            position={{ y: -10 }}
+                        />
+                        <Line
+                            type="monotone"
+                            dataKey={`1.${timeline.stat}`}
+                            stroke="hsl(0, 100%, 55%)"
+                            activeDot={{ stroke: 'hsl(0, 100%, 55%)', fill: 'hsl(0, 100%, 55%)' }}
+                            dot={false}
+                        />
+                        <Line
+                            type="monotone"
+                            dataKey={`2.${timeline.stat}`}
+                            stroke="hsl(240, 80%, 55%)"
+                            activeDot={{ stroke: 'hsl(240, 80%, 55%)', fill: 'hsl(240, 80%, 55%)' }}
+                            dot={false}
+                        />
+                    </LineChart>
+                </ResponsiveContainer>
+                <div className="TimelineArea__chart-selector">
+                    {Object.entries(timelineStatCategories).map(([statKey, statNames]) => (
+                        <button
+                            className={`TimelineArea__chart-stat ${timeline.stat === statKey ? 'TimelineArea__chart-stat--active' : ''}`}
+                            onClick={() => {
+                                timeline.setStat(statKey);
+                                localStorage.timelineStat = statKey;
+                            }}
+                        >
+                            {statNames[0]}
+                        </button>
+                    ))}
+                </div>
+                {metrics &&
+                    Object.entries(metrics).map(([statKey, values]) => { // eslint-disable-line arrow-body-style
+                        // const aheadOrBehind = values.summary.ahead - values.summary.behind >= 0 ? 'ahead' : 'behind';
+                        // const avgAheadOrBehind = values.summary.ahead - values.summary.behind >= 0 ? values.summary.avgAhead : values.summary.avgBehind;
+
+                        return (
+                            <div className={`TimelineArea__metric TimelineArea__metric--${statKey}`}>
+                                <h3 className="TimelineArea__metric-stat">
+                                    {timelineStatCategories[statKey][0]}
+                                </h3>
+                                <ResponsiveContainer className="TimelineArea__metric-chart" width="80%" height={50}>
+                                    <LineChart data={values.data} margin={{ top: 5, bottom: 5 }}>
+                                        <ReferenceLine y={0} stroke="hsl(0, 0%, 47%)" strokeWidth={0.5} />
+                                        <YAxis domain={[-1.05, 1.05]} allowDataOverflow hide />
+                                        <Line
+                                            type="monotone"
+                                            dataKey="value"
+                                            stroke="hsl(210, 68%, 47%)"
+                                            strokeWidth={2}
+                                            dot={false}
+                                        />
+                                    </LineChart>
+                                </ResponsiveContainer>
+                                {/* <p className="TimelineArea__metric-value">
+                                    You were {aheadOrBehind} {values.summary[aheadOrBehind]}% of the time
+                                </p>
+                                <p className="TimelineArea__metric-value">
+                                    On average, {aheadOrBehind} by {avgAheadOrBehind[0]}
+                                    &nbsp;({aheadOrBehind === 'ahead' ? '' : '-'}{avgAheadOrBehind[1]}%)
+                                </p>
+                                <p className="TimelineArea__metric-value">
+                                    {values.summary.avgLeadLag[0] >= 0 ? 'Led' : 'Lagged'} by&nbsp;
+                                    {Math.abs(values.summary.avgLeadLag[0])}
+                                    &nbsp;({values.summary.avgLeadLag[0] >= 0 ? '+' : ''}
+                                    {values.summary.avgLeadLag[1]}%) on average
+                                </p> */}
+                            </div>
+                        );
+                    })}
+            </div>
+            {currentTimelineState &&
+                <div className="timeline-state">
+                    <div className="timeline-state__players">
+                        <div className="timeline-state__central">
+                            <div className="timeline-state__player-supply timeline-state__player-supply--player1">
+                                <div className="timeline-state__supply-value">
+                                    {currentTimelineState[1].supply} / {currentTimelineState[1].supply_cap}
+                                </div>
+                                <div className="timeline-state__supply-block-value">
+                                    {currentTimelineState[1].supply_block}s
+                                </div>
+                            </div>
+                            <span className="timeline-state__current-time">
+                                {formatCurrentTime(gameloop.current)}
+                            </span>
+                            <div className="timeline-state__player-supply timeline-state__player-supply--player2">
+                                <div className="timeline-state__supply-value">
+                                    {currentTimelineState[2].supply} / {currentTimelineState[2].supply_cap}
+                                </div>
+                                <div className="timeline-state__supply-block-value">
+                                    {currentTimelineState[2].supply_block}s
+                                </div>
+                            </div>
+                        </div>
+                        {Object.values(players).map((player, index) => (
+                            <div
+                                key={`player-${player.mmr}`}
+                                className={`timeline-state__player timeline-state__player--player${index + 1}`}
+                            >
+                                {index !== Object.values(players).length - 1 ?
+                                    <Fragment>
+                                        <img
+                                            src={`../../icons/${player.race.toLowerCase()}-logo.svg`}
+                                            alt={player.race}
+                                            className="timeline-state__race-icon"
+                                        />
+                                        <div className="timeline-state__player-info">
+                                            <span className="timeline-state__player-name">
+                                                {player.name}
+                                            </span>
+                                            <span className="timeline-state__player-mmr">
+                                                Player {index + 1}&nbsp;&nbsp;{player.mmr}
+                                            </span>
+                                        </div>
+                                    </Fragment>
+                                    :
+                                    <Fragment>
+                                        <div className="timeline-state__player-info" style={{ textAlign: 'right' }}>
+                                            <span className="timeline-state__player-name">
+                                                {player.name}
+                                            </span>
+                                            <span className="timeline-state__player-mmr">
+                                                Player {index + 1}&nbsp;&nbsp;{player.mmr}
+                                            </span>
+                                        </div>
+                                        <img
+                                            src={`../../icons/${player.race.toLowerCase()}-logo.svg`}
+                                            alt={player.race}
+                                            className="timeline-state__race-icon"
+                                        />
+                                    </Fragment>
+                                }
+                            </div>
+                        ))}
+                    </div>
+                    <RaceState
+                        timelineState={currentTimelineState}
+                        players={players}
+                    />
+                    {/* <CurrentSelectionState
+                        timelineState={currentTimelineState}
+                        players={players}
+                    /> */}
+                    <UpgradeState
+                        timelineState={currentTimelineState}
+                        players={players}
+                    />
+                    <ObjectState
+                        objectType="unit"
+                        timelineState={currentTimelineState}
+                        players={players}
+                        visibleState={visibleState}
+                        objectStates={objectStates.unit}
+                        ignoreObjects={ignoreObjects.unit}
+                    />
+                    <ObjectState
+                        objectType="building"
+                        timelineState={currentTimelineState}
+                        players={players}
+                        visibleState={visibleState}
+                        objectStates={objectStates.building}
+                        ignoreObjects={ignoreObjects.building}
+                    />
+                </div>}
+        </div>
+    );
+};
+
+export default TimelineArea;
